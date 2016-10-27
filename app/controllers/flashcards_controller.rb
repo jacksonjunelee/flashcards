444e1755255baef5a24c500662f439680346f624
jacksonjunelee <jjlee5266@gmail.com>
2016-10-26 21:25:43 -0400

    created CRUD routes for flashcards

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -1,4 +1,47 @@
 class FlashcardsController < ApplicationController
-  def index
+
+  def show
+    @flashcard = Flashcard.find(params[:id])
+  end
+
+  def new
+    @flashcard = Flashcard.new
+  end
+
+  def create
+    flashcard = Flashcard.new(flashcard_params)
+    if flashcard.save
+      redirect_to(flashcard_path(flashcard))
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @flashcard = Flashcard.find(params[:id])
+  end
+
+  def update
+    flashcard = Flashcard.find(params[:id])
+    if flashcard.update(flashcard_params)
+      redirect_to(flashcard_path(flashcard))
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  flashcard = Flashcard.find(params[:id])
+    if flashcard.destroy
+      redirect_to('/')
+    else
+      redirect_to edit_flashcard_path(flashcard)
+    end
+  end
+
+  private
+
+  def flashcard_params
+    params.require(:flashcard).permit(:question, :answer)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
   root 'welcome#index'
-  resources :flashcards, only: [:index, :show, :destroy]
+  resources :flashcards, except: ['index']
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
