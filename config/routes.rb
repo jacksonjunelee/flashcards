69fb1607cccfda01e3dd57654a129a9d99dff175
jacksonjunelee <jjlee5266@gmail.com>
2016-10-26 20:43:15 -0400

    added welcome landing page to start creating flashcards

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  root 'welcome#index'
+  resources :flashcards, only: [:index, :show, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
